eb89aee709f733f4d56d5c74e1ac106888f46b07
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-22 22:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function takeANumber(katzDeli,name){
   }
 return 'Welcome, '+name+'. You are number '+(katzDeli.length) +' in line.'
 }
+
+
+/*
+2. Build a function `nowServing`. This function should return the first person in line
+ and then remove that individual from the line. If there is nobody in line,
+  it should return "There is nobody waiting to be served!"
+
+*/
+
+function nowServing(katzDeli){
+
+  if(katzDeli.length == 0){
+    var person = katzDeli[0]
+    return 'Currently serving '+person+'.';
+  }else {
+    return  "There is nobody waiting to be served!"
+  }   
+}

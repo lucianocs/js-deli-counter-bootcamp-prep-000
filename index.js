580b7ecd30e6dbd338f741618b81bf98766a645c
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-22 22:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var katzDeli = [];
 
-function takeANumber(name){
+function takeANumber(katzDeli,name){
 
   if (katzDeli.length ==0) {
     katzDeli[0] = name
   }else {
     katzDeli.push(name)
   }
+return 'Welcome, '+ +'. You are number '+(katzDeli.length) +' in line.'  
 }
-
-

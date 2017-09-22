d27af61abf2ca5a72c27953e40f3b7be1348d393
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-22 23:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function nowServing(katzDeli){
   }
 }
 
-//  3. Build a function `currentLine` that returns the current line. For example, 
+//  3. Build a function `currentLine` that returns the current line. For example,
 // if `katzDeliLine` is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)`
 // would return `"The line is currently: 1. Ada, 2. Grace"`.  If there is nobody
 // in line, it should return `"The line is currently empty."`
@@ -33,9 +33,9 @@ function currentLine(katzDeli){
 
   for(i = 1;i == line.length;i++ ){
     line = line +  i.toString() + '. '+katzDeli[i-1]+', '
-    
+
   }
 
 
-  
+
 }
